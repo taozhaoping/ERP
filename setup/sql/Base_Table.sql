0129e3d7975d3f5344e2041b5833380ed3f9d05d
陶照平 <taozhaoping@gmail.com>
2015-03-27 15:14:01 +0800

    部门表

--- setup/sql/Base_Table.sql
+++ setup/sql/Base_Table.sql
@@ -1,6 +1,9 @@
 alter table SYS_BASI_TYPE drop primary key cascade;
 ALTER TABLE SYS_DICTIONARY DROP PRIMARY KEY CASCADE;
 alter table sys_Warehouse drop primary key cascade;
+alter table SYS_DEPT drop primary key cascade;
+
+drop table SYS_DEPT cascade constraints;
 
 drop table sys_Warehouse cascade constraints;
 DROP TABLE SYS_DICTIONARY CASCADE CONSTRAINTS;
@@ -10,6 +13,13 @@ drop sequence SEQUENCE_sys_Warehouse;
 
 
 create sequence SEQUENCE_sys_Warehouse
+start with 1
+ maxvalue 999999999
+ minvalue 1
+ cache 10
+order;
+
+create sequence SEQUENCE_SYS_DEPT
 start with 1
  maxvalue 999999999
  minvalue 1
@@ -85,4 +95,37 @@ COMMENT ON COLUMN SYS_WAREHOUSE.ADDRESS IS
 
 ALTER TABLE SYS_WAREHOUSE
    ADD CONSTRAINT PK_SYS_WAREHOUSE PRIMARY KEY (ID);
+
+/*==============================================================*/
+/* Table: 部门                                                                                                                                                          */
+/*==============================================================*/
+create table SYS_DEPT 
+(
+   ID                   NUMBER               not null,
+   DEPT_NAME            VARCHAR(50),
+   ENTERPRISE_ID        NUMBER,
+   TYPE                 NUMBER,
+   enabled            NUMBER
+);
+
+comment on table SYS_DEPT is
+'部门配置';
+
+comment on column SYS_DEPT.ID is
+'主键';
+
+comment on column SYS_DEPT.DEPT_NAME is
+'部门名称';
+
+comment on column SYS_DEPT.ENTERPRISE_ID is
+'所属公司';
+
+comment on column SYS_DEPT.TYPE is
+'部门类型';
+
+comment on column SYS_DEPT.enabled is
+'状态';
+
+alter table SYS_DEPT
+   add constraint PK_SYS_DEPT primary key (ID);
 
