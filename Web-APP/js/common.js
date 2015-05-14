da51078f6f38a95368433c305d84494f10c664d3
caiyingying <caiyingying234@gmail.com>
2015-05-14 17:15:07 +0800

    js添加分号

--- Web-APP/js/common.js
+++ Web-APP/js/common.js
@@ -161,21 +161,20 @@ function fillUserList(userList, id, type) {
 	}
 }
 
-Date.prototype.format = function(format)
-{
-var o = {
-            "M+" : this.getMonth()+1, //month
-            "d+" : this.getDate(), //day
-            "h+" : this.getHours(), //hour
-            "m+" : this.getMinutes(), //minute
-            "s+" : this.getSeconds(), //second
-            "q+" : Math.floor((this.getMonth()+3)/3), //quarter
-            "S" : this.getMilliseconds() //millisecond
-        }
+Date.prototype.format = function(format) {
+	var o = {
+	            "M+" : this.getMonth()+1, //month
+	            "d+" : this.getDate(), //day
+	            "h+" : this.getHours(), //hour
+	            "m+" : this.getMinutes(), //minute
+	            "s+" : this.getSeconds(), //second
+	            "q+" : Math.floor((this.getMonth()+3)/3), //quarter
+	            "S" : this.getMilliseconds() //millisecond
+	        };
     if(/(y+)/.test(format))
     format=format.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
     for(var k in o)
     if(new RegExp("("+ k +")").test(format))
     format = format.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
     return format;
-}
+};
