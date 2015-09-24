aa40635fd4630e7406977d1e249c1cd3ab054fcd
26078 <26078@win260780922.dahuatech.com>
2015-09-24 11:36:04 +0800

    数据库创建语句

--- setup/sql/createnamespace.sql
+++ setup/sql/createnamespace.sql
@@ -1,11 +1,18 @@
--- Create the user 
-create user ERP
+--创建数据库
+CREATE TABLESPACE ERP_ORCL
+LOGGING
+DATAFILE 'F:\oracle\oradata\orcl\ERP_DATA01.DBF' 
+SIZE 300M 
+AUTOEXTEND ON 
+NEXT 100M MAXSIZE UNLIMITED
+EXTENT MANAGEMENT LOCAL;
+--创建用户
+create user ERP identified by agile932
   default tablespace ERP_ORCL
   temporary tablespace TEMP
   profile DEFAULT
   password expire
   quota unlimited on erp_orcl;
--- Grant/Revoke system privileges 
 grant create any job to ERP;
 grant create any procedure to ERP;
 grant create any sequence to ERP;
@@ -19,3 +26,4 @@ grant drop any table to ERP;
 grant drop any view to ERP;
 grant insert any table to ERP;
 grant update any table to ERP;
+
