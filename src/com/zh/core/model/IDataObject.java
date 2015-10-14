37f59dba0a4f5fcd4b4428757c6a8d9be186a5d2
26078 <26078@win260780922.dahuatech.com>
2015-10-14 20:17:50 +0800

    1.采购需求清单，排序问题修改 2.所有菜单默认排序为1(id) 降序

--- src/com/zh/core/model/IDataObject.java
+++ src/com/zh/core/model/IDataObject.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * 数据对象接口
+ * 
  * @author Flouny.Caesar
  *
  */
@@ -13,7 +14,7 @@ public abstract class IDataObject implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -4051045946051357056L;
-	private String orderByClause;
+	private String orderByClause = "1 DESC";
 
 	public String getOrderByClause() {
 		return orderByClause;
