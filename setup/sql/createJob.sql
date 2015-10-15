d1f120ef47a12a8f67d821fdf066286fab9daf15
26078 <26078@win260780922.dahuatech.com>
2015-10-15 17:04:41 +0800

    1.检查采购需求订单是否完成

--- setup/sql/createJob.sql
+++ setup/sql/createJob.sql
@@ -22,50 +22,53 @@ drop  procedure erp.task_purchasing_demand;
 
 create or replace procedure task_Purchasing_demand as
 begin
-declare
-       isProd  number; -- 所有采购是否采购完成
-begin
-       --获取状态在处理中的所有采购需求头表数据
-      for sub in (select id,order_id from t_procurement_demand_primary where status = 1) loop
-        isProd  := 0;
-        dbms_output.PUT_LINE(isProd);
-          for demandObject in (
-         select p.order_id as orderID,
-       s.ProductsID,
-       isMainProducts,
-       Demand_Number,
-       nvl(sp.storageNumber,0) storageNumber
-  from T_Procurement_Demand_DETAIL S
-  LEFT JOIN t_Procurement_Demand_Primary p
-    on s.productsid = p.id
-  left join (select procurementid,
-                    productsid,
-                    nvl(sum(purchase_number),0) storageNumber
-               from t_purchaseorder_detail pd
-               left join t_purchaseorder_primary pp
-                 on pd.purchaseorderid = pp.id
-              where pp.status = 3
-              group by procurementid, productsid) sp
-    on s.id = sp.procurementid
- where ProcuremenID = sub.id ) loop
-                   --处理数据
-                 if(isProd = 0) then
-                   if (demandObject.Demand_Number != demandObject.storageNumber) then
-                     isProd  := 1;
-                     dbms_output.PUT_LINE('未完成');
-                     exit;
-                   end if;
-                 end if;
-          end loop;
-          if(isProd = 0) then
-               --修改当前采购需求单状态
-               update t_Procurement_Demand_Primary set status = 2 where id = sub.id;
-               update T_SALES_ORDER_PRIMARY set status=2 where id=sub.order_id;
-                dbms_output.PUT_LINE('完成');
-               commit;
+  declare
+    isProd number; -- 所有采购是否采购完成
+  begin
+    --获取状态在处理中的所有采购需求头表数据
+    for sub in (select id, order_id
+                  from t_procurement_demand_primary
+                 where status = 1) loop
+      isProd := 0;
+      dbms_output.PUT_LINE(isProd);
+      for demandObject in (select dd.ProductsID,
+                                  isMainProducts,
+                                  Demand_Number,
+                                  nvl(v.storageNumber, 0) storageNumber
+                             from t_procurement_demand_detail dd
+                             left join (select pd.procurementid,
+                                              sum(sd.storage_number) storageNumber
+                                         from t_purchaseorder_detail pd
+                                         left join t_purchaseorder_primary pp
+                                           on pd.purchaseorderid = pp.id
+                                         left join t_storage_detail sd
+                                           on pd.id = sd.purchaseorderid
+                                        where pp.status = 3
+                                        group by pd.procurementid) v
+                               on dd.id = v.procurementid
+                            where dd.ProcuremenID = sub.id) loop
+        --处理数据
+        if (isProd = 0) then
+          if (demandObject.storageNumber<demandObject.Demand_Number) then
+            isProd := 1;
+            dbms_output.PUT_LINE('未完成');
+            exit;
           end if;
+        end if;
       end loop;
-end;
+      if (isProd = 0) then
+        --修改当前采购需求单状态
+        update t_Procurement_Demand_Primary
+           set status = 2
+         where id = sub.id;
+        update T_SALES_ORDER_PRIMARY
+           set status = 2
+         where id = sub.order_id;
+        dbms_output.PUT_LINE('完成');
+        commit;
+      end if;
+    end loop;
+  end;
 end task_Purchasing_demand;
 /
 
